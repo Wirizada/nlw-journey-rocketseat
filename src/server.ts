6c3fe32faaa99da4640ce3b83aa6984821a5a49f
Wirizada <ghostybrine@gmail.com>
2024-07-14 18:07:06 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import fastify from "fastify";
+import { createTrip } from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from "@fastify/cors";
+import { confirmPaticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-links";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDatails } from "./routes/get-trip-datails";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./error-handler";
+import { env } from "./env";
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler(errorHandler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmPaticipants);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDatails);
+app.register(getParticipant);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server running");
+});
